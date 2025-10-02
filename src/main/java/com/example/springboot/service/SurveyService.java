6b35045937d3ade9e7665abb36ecf47cabe385d3
Devil-dxt <133145761+Devil-dxt@users.noreply.github.com>
2025-10-02 21:17:38 +0800

    Add files via upload

---
+++ src/main/java/com/example/springboot/service/SurveyService.java
@@ -0,0 +1,15 @@
+package com.example.springboot.service;
+
+import com.example.springboot.dto.FirstSurveyRequest;
+import com.example.springboot.dto.RetestSurveyRequest;
+import com.example.springboot.dto.SurveyResponse;
+import com.example.springboot.entity.StudentEvaluateResult;
+
+import java.util.List;
+
+public interface SurveyService {
+    SurveyResponse handleFirstSurvey(FirstSurveyRequest req);
+    SurveyResponse handleRetestSurvey(RetestSurveyRequest req);
+    List<StudentEvaluateResult> getResultsByStudent(String studentId);
+}
+
